05166d5c32771f85d854d3832d18a312d3b93112
Stehvin <stehvin.olson@gmail.com>
2018-01-20 18:37:44 -0600

    Format code to better conform to PEP 8 sytle guide.

--- boothSetupInfo.py
+++ boothSetupInfo.py
@@ -1,30 +1,31 @@
 from openpyxl import load_workbook
 
+
 def boothInfo(booth):
     """Find the parameters for a specific booth, which are listed in
        the excel document described by path (below).
     """
 
-    # path to excel document
+    # Get path to excel document.
     path = r'C:\Users\Example\UTool Records.xlsx'
 
-    # load excel workbook
+    # Load excel workbook.
     try:
         wb = load_workbook(filename=path, data_only=True)
         ws = wb.get_sheet_by_name('Sheet1')
     except:
         return None
 
-    # initialize booth info dictionaries
+    # Initialize booth info dictionaries.
     tbl = {}
     utool = {}
     Euler = {}
 
-    # find proper row
+    # Find proper row.
     for row in ws.iter_rows('A4:N{}'.format(ws.max_row)):
         if row[0].value == booth:
             
-            # utool set points
+            # Get Utool set points.
             utool['x'] = row[2].value
             utool['y'] = row[3].value
             utool['z'] = row[4].value
@@ -32,46 +33,46 @@ def boothInfo(booth):
             utool['p'] = 0
             utool['r'] = 0
             
-            # table center point
+            # Get table center point.
             tbl['x'] = row[5].value
             tbl['y'] = row[6].value
             tbl['z'] = row[7].value
 
-            # euler angles
+            # Get Euler angles.
             Euler['w'] = row[8].value
             Euler['p'] = row[9].value
             Euler['r'] = row[10].value
 
-            # copy dictionaries
+            # Copy dictionaries.
             utoolCopy = dict(utool)
             tblCopy = dict(tbl)
             EulerCopy = dict(Euler)
 
-            # dust collector location
+            # Get dust collector location and angle.
             dustColLoc = row[11].value
-
-            # dust collector angle
             dustColAng = row[12].value
 
-            # configuration string
+            # Get configuration string.
             configStr = row[13].value
             break
     return utoolCopy, tblCopy, EulerCopy, dustColLoc, dustColAng, configStr
 
+
 def curUtool(booth, curUtoolNum):
-    """Returns the utool setpoints for the given booth and utool number.
+    """Returns the utool setpoints for the given booth and utool
+    number.
     """
-    # path to excel document
+    # Get path to excel document.
     path = r'C:\Users\Example\UTool Records.xlsx'
 
-    # load excel workbook
+    # Load excel workbook.
     try:
         wb = load_workbook(filename=path, data_only=True)
         ws = wb.get_sheet_by_name(str(booth))
     except:
         return None
 
-    # find current point/booth utool
+    # Find current point/booth Utool.
     curUtool = {}
     for row in ws.iter_rows('A2:G{}'.format(ws.max_row)):
         if row[0].value == curUtoolNum:
@@ -84,20 +85,22 @@ def curUtool(booth, curUtoolNum):
             break
     return curUtool
 
+
 def curUframe(booth, curUframeNum):
-    """Returns the uframe setpoints for the given booth and uframe number.
+    """Returns the uframe setpoints for the given booth and uframe
+    number.
     """
-    # path to excel document
+    # Get path to excel document.
     path = r'C:\Users\Example\UTool Records.xlsx'
 
-    # load excel workbook
+    # Load excel workbook.
     try:
         wb = load_workbook(filename=path, data_only=True)
         ws = wb.get_sheet_by_name(str(booth))
     except:
         return None
 
-    # find current point/booth utool
+    # Find current point/booth Utool.
     curUframe = {}
     for row in ws.iter_rows('K2:Q{}'.format(ws.max_row)):
         if row[0].value == curUframeNum:
